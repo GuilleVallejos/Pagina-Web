ebdc3870b4c0a7d754c28aac051038349d2deab5
Guillermo Vallejos <guille971172@gmail.com>
2019-01-15 22:20:28 -0300

    Creacion de script y configuracion de footer

---
+++ scriptPrincipal.js
@@ -0,0 +1,11 @@
+$(document).on("ready",configurarApp);
+
+function configurarApp()
+{
+    var canvas = document.getElementById("miCanvas");
+    var ctx = canvas.getContext("2d");
+    canvas.width = screen.availWidth;
+    
+
+
+}
