b9ed3c00eb0ca2f27b5eafad818e818277940b26
Guillermo Vallejos <guille971172@gmail.com>
2019-01-15 22:38:09 -0300

    Se configuro el canvas

--- scriptPrincipal.js
+++ scriptPrincipal.js
@@ -5,7 +5,14 @@ function configurarApp()
     var canvas = document.getElementById("miCanvas");
     var ctx = canvas.getContext("2d");
     canvas.width = screen.availWidth;
-    
+    dibujarFooter(canvas,ctx);
+}
+function dibujarFooter(canvas, contexto)
+{
+    contexto.fillStyle="rgba(0,0,0,0.8)";
+    contexto.moveTo(0,0);
+    contexto.quadraticCurveTo(0,-100,canvas.width-450, canvas.height-50);
+    contexto.fill();
 
-
+    //quadraticCurveTo(cpx,cpy,x,y) según W3C
 }
